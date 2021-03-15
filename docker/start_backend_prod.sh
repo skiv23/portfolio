9592b1b03b7e14d1eec04899020c1ab367ea6d14
Sergey Bondar <sbondar@milosolutions.com>
2021-03-15 14:12:57 +0300

    fix configs

--- docker/start_backend_prod.sh
+++ docker/start_backend_prod.sh
@@ -3,7 +3,7 @@
 apt-get update -y
 apt-get install -y uwsgi-plugin-python3
 cd /code
-pip install -r backend/requirements/production.txt
+pip install -r requirements/production.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
 uwsgi --ini /code/docker/portfolio.ini
